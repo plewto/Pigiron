97c5ac6b7ae672dd1b9748b711d052939b9f7011
Steven Jones <plewto@gmail.com>
2021-06-05 11:59:18 -0500

    Removed gomidi related files.

--- main.go
+++ main.go
@@ -2,37 +2,11 @@ package main
 
 import (
 	"fmt"
-
-	midi "github.com/plewto/pigiron/midi"
-	op "github.com/plewto/pigiron/op"
-	
-
 )
 
 func main() {
 	fmt.Println("Pigiron.main()")
-	midi.DumpDevices()
-	fmt.Println("")
-	a, _ := op.MakeMIDIInput("a", "Arturia")
-	c, _ := op.MakeOperator("ChannelFilter", "filter")
-	mon, _ := op.MakeOperator("Monitor", "monitor")
 
-	// Enable all channels
-	for i:=1; i<17; i++ {
-		c.SelectChannel(i)
-	}
-
-	a.Connect(c)
-	c.Connect(mon)
-	
-	fmt.Println(c)
-
-	
-	fmt.Println("Ready....")
-	for {}
-
-	
-	
 	Cleanup()
 
 }
@@ -42,7 +16,4 @@ func Ignore(values ...interface{}) {}
 
 func Cleanup() {
 	fmt.Println("pigiron.Cleanup() executes")
-	op.Cleanup()
-	midi.Cleanup()
-	
 }

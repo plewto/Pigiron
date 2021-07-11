42609fcba8174ffa5f72e574dbd9f2e2c2566ba0
Steven Jones <plewto@gmail.com>
2021-07-10 19:21:26 -0500

    Moves error utilities to smf.go

--- smf/expect.go
+++ smf/expect.go
@@ -4,22 +4,6 @@ import (
 	"fmt"
 )
 
-func exError(message string, more ...string) error {
-	msg := fmt.Sprintf("ERROR: %s", message)
-	for _, s := range more {
-		msg += fmt.Sprintf("\nERROR: %s", s)
-	}
-	return fmt.Errorf(msg)
-}
-
-func compoundError(err1 error, message string, more ...string) error {
-	msg := fmt.Sprintf("ERROR: %s\n", message)
-	for _, s := range more {
-		msg += fmt.Sprintf("\nERROR: %s", s)
-	}
-	msg += fmt.Sprintf("PREVIOUS ERROR:\n%s", err1)
-	return fmt.Errorf(msg)
-}
 
 // expectChunkID checks byte buffer for specific chunk ID.
 // buffer - the data
--- smf/smf.go
+++ smf/smf.go
@@ -1,2 +1,18 @@
 package smf
 
+func exError(message string, more ...string) error {
+	msg := fmt.Sprintf("ERROR: %s", message)
+	for _, s := range more {
+		msg += fmt.Sprintf("\nERROR: %s", s)
+	}
+	return fmt.Errorf(msg)
+}
+
+func compoundError(err1 error, message string, more ...string) error {
+	msg := fmt.Sprintf("ERROR: %s\n", message)
+	for _, s := range more {
+		msg += fmt.Sprintf("\nERROR: %s", s)
+	}
+	msg += fmt.Sprintf("PREVIOUS ERROR:\n%s", err1)
+	return fmt.Errorf(msg)
+}

1186a95ff003af2c7f7bf18e3f2b55d1b4340112
Steven Jones <plewto@gmail.com>
2021-07-11 23:50:08 -0500

    Adds ResourceFilename function.

--- config/config.go
+++ config/config.go
@@ -26,7 +26,7 @@ var (
 	tomlTree *toml.Tree
 )
 
-// subUserHome substitutes leading ~ charater for user home directory.
+// subUserHome substitutes leading '~' character for user home directory.
 //
 func subUserHome(filename string) string {
 	result := filename
@@ -164,6 +164,33 @@ func readConfigurationFile(filename string) {
 		GlobalParameters.ErrorColor = getColor(readString("colors.error", ""))
 	}
 }
+
+
+// ResourceFilename returns filename relative to the resources directory.
+// The resources directory is located at <config>/resources/
+// On Linux this location is ~/.config/pigiron/resources/
+//
+// Returns non-nil error if resources directory can not be determined.
+//
+// Example:
+// ResourceFilename("foo", "bar.txt") --> ~/.config/pigiron/resources/foo/bar.txt
+//
+func ResourceFilename(elements ...string) (string, error) {
+	cfigdir, err := os.UserConfigDir()
+	if err != nil {
+		msg := "ERROR: Resource filename can not be determined.\n"
+		msg += "ERROR: Can not determine configuration directory location.\n"
+		msg += fmt.Sprintf("ERROR: %s\n", err)
+		err = fmt.Errorf(msg)
+		return "", err
+	}
+	acc := filepath.Join(cfigdir, "pigiron", "resources")
+	for _, e := range elements {
+		acc = filepath.Join(acc, e)
+	}
+	return acc, err
+}
+
 
 func init() {
 	defineColors()

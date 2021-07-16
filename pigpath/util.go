6076b518fdbf18d7337fe6a28af8a3c11f118200
Steven Jones <plewto@gmail.com>
2021-07-15 23:31:10 -0500

    Adds pigpath filename utilities.

---
+++ pigpath/util.go
@@ -0,0 +1,89 @@
+/* 
+ * pigpath package provides utilities for for making filename substitutions.
+ * Specifically leading characters '~' and '!' are replaced with the user's
+ * home and pigiron configuration directories respectively.
+ */
+
+package pigpath
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+
+// issuer: Refactor
+// ResourceFilename returns filename relative to the resources directory.
+// The resources directory is located at <config>/resources/
+// On Linux this location is ~/.config/pigiron/resources/
+//
+// Returns non-nil error if resources directory can not be determined.
+//
+// Example:
+// ResourceFilename("foo", "bar.txt") --> ~/.config/pigiron/resources/foo/bar.txt
+//
+func ResourceFilename(elements ...string) (string, error) {
+	cfigdir, err := os.UserConfigDir()
+	if err != nil {
+		msg := "ERROR: Resource filename can not be determined.\n"
+		msg += "ERROR: Can not determine configuration directory location.\n"
+		msg += fmt.Sprintf("ERROR: %s\n", err)
+		err = fmt.Errorf(msg)
+		return "", err
+	}
+	acc := filepath.Join(cfigdir, "pigiron", "resources")
+	for _, e := range elements {
+		acc = filepath.Join(acc, e)
+	}
+	return acc, err
+}
+
+
+func UserHomeDir() string {
+	dir, _ := os.UserHomeDir()
+	return dir
+}
+
+func UserConfigDir() string {
+	dir, _ := os.UserConfigDir()
+	return filepath.Join(dir, "pigiron")
+}
+
+
+// subSpecialDirectories replaces leading ~ and ! characters in filename
+// '~' is replaced with user's home dir.
+// '!' is replaced with pigiron configuration dir.
+//
+func subSpecialDirectories (filename string) string {
+	result := filename
+	if len(filename) > 0 {
+		switch {
+		case filename[0] == '~':
+			home := UserHomeDir()
+			result = filepath.Join(home, filename[1:])
+		case filename[0] == '!':
+			cnfig := UserConfigDir()
+			result = filepath.Join(cnfig, filename[1:])
+		default:
+			// ignore
+		}
+	}
+	return result
+}
+
+
+// Join concatenates filepath components into string.
+// It is like the standard filepath.Join but substitutes leading
+// ~ and ! characters with user's home and pigiron configuration
+// directories respectively.
+// 
+func Join(base string, elements ...string) string {
+	var acc = make([]string,1,12)
+	acc[0] = subSpecialDirectories(base)
+	for _, e := range elements {
+		acc = append(acc, e)
+	}
+	return filepath.Join(acc...)
+}
+	
---
+++ pigpath/util_test.go
@@ -0,0 +1,44 @@
+package pigpath
+
+import (
+	"testing"
+	"fmt"
+	"path/filepath"
+)
+
+
+func TestPigpath(t *testing.T) {
+	fmt.Print("")
+	
+	home := UserHomeDir()
+	config := UserConfigDir()
+
+	a := "~/foo"
+	b := subSpecialDirectories(a)
+	if b[0:len(home)] != home {
+		t.Fatalf("Expected filename start to be user's home: got '%s'", b)
+	}
+
+	a = "!/foo"
+	b = subSpecialDirectories(a)
+
+	if b[0:len(config)] != config {
+	 	t.Fatalf("Expected filename start to be configuration directory, got '%s'", b)
+	 }
+	
+	b = Join("~", "alpha", "bar")
+	expect := filepath.Join(home, "alpha", "bar") 
+	if b != expect {
+		t.Fatalf("Expected filenem.Join to return '%s', got '%s'", expect, b)
+	}
+	
+	result := Join("~")
+	if result != home {
+		t.Fatalf("Expected Join(\"~\") to return '%s', got '%s'\n", home, result)
+	}
+
+	result = Join("!")
+	if result != config {
+		t.Fatalf("Expected Join(\"~\") to return '%s', got '%s'\n", config, result)
+	}
+}

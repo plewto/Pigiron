cdfe48cf90913d65e79097a470c3f1a3d8159109
Steven Jones <plewto@gmail.com>
2021-07-20 04:05:56 -0500

    Adds consistent errors to convertTrackBytes.

--- midi/track.go
+++ midi/track.go
@@ -57,12 +57,25 @@ func convertTrackBytes(bytes []byte) (track *SMFTrack, err error) {
 	var runningChannel MIDIChannelNibble
 	var index int = 0 // FOR error reports only.
 	track = &SMFTrack{make([]*UniversalEvent, 0, 512)}
+
+	// creates simple error 
+	var error1 = func(text string) error {
+		msg1 := fmt.Sprintf("convertTrackBytes() at index %d", index)
+		return pigerr.New(msg1, text)
+	}
+
+	// creates compound error
+	var error2 = func(err error, text string) error {
+		msg1 := fmt.Sprintf("convertTrackBytes() at index %d", index)
+		return pigerr.CompoundError(err, msg1, text)
+	}
+	
 	for len(bytes) > 0 {
 		var vlq *VLQ
 		vlq, bytes, err = takeVLQ(bytes)
 		if err != nil {
-			errmsg := "convertTrackBytes takeVLQ() error  at index %d"
-			err = pigerr.CompoundError(err, fmt.Sprintf(errmsg, index))
+			errmsg := "Invalid delta-time"
+			err = error2(err, errmsg)
 			return
 		}
 		index += vlq.Length()
@@ -70,9 +83,8 @@ func convertTrackBytes(bytes []byte) (track *SMFTrack, err error) {
 		var status byte
 		status, bytes, err = takeByte(bytes)
 		if err != nil {
-			errmsg := "convertTrackBytes expected status byte "
-			errmsg += "(or running status) at index %d"
-			err = pigerr.CompoundError(err, fmt.Sprintf(errmsg, index))
+			errmsg := "Expected status byte or running-status data byte"
+			err = error2(err, errmsg)
 			return
 		}
 		index++
@@ -80,24 +92,22 @@ func convertTrackBytes(bytes []byte) (track *SMFTrack, err error) {
 		case status & 0x80 == 0: // using running status
 			dataCount, _ := channelStatusDataCount[StatusByte(runningStatus)]
 			if dataCount == 0 {
-				errmsg1 := "convertTrackBytes running status dataCount = 0"
-				errmsg2 := "runningStatus = 0x%02X %s, index = %d"
-				err = pigerr.New(errmsg1, fmt.Sprintf(errmsg2, byte(runningStatus), runningStatus, index))
-				return
+				errmsg := "Illegal runningStatus 0x%02X"
+				err = error1(fmt.Sprintf(errmsg, byte(runningStatus)))
 			}
 			d1, d2 := byte(0), byte(0)
 			d1, bytes, err = takeByte(bytes)
 			if err != nil {
-				errmsg := "convertTrackBytes get running status data byte 1 error, index = %d"
-				err = pigerr.CompoundError(err, fmt.Sprintf(errmsg, index))
+				errmsg := "error geting running-status first data byte"
+				err = error2(err, errmsg)
 				return
 			}
 			index++
 			if dataCount == 2 {
 				d2, bytes, err = takeByte(bytes)
 				if err != nil {
-					errmsg := "convertTrackBytes get running status data byte 2 error, index = %d"
-					err = pigerr.CompoundError(err, fmt.Sprintf(errmsg, index))
+					errmsg := "error getting running-status second data byte."
+					err = error2(err, errmsg)
 					return
 				}
 				index++
@@ -105,10 +115,9 @@ func convertTrackBytes(bytes []byte) (track *SMFTrack, err error) {
 			var ue *UniversalEvent
 			ue, err = MakeChannelEvent(runningStatus, runningChannel, d1, d2)
 			if err != nil {
-				errmsg1 := "convertTrackByets error while creating running-status message "
-				errmsg2 := "runningStatus = 0x%02X  runningChannel = %d,  index = %d"
-				err = pigerr.CompoundError(err, errmsg1, fmt.Sprintf(errmsg2, byte(runningStatus), byte(runningChannel), index))
-				return
+				errmsg := "creating running-status message, runningStatus = 0x%02X, runningChannel = %d"
+				err = error2(err, fmt.Sprintf(errmsg, byte(runningStatus), byte(runningChannel)))
+				return 
 			}
 			ue.deltaTime = deltaTime
 			events = append(events, ue)
@@ -119,18 +128,16 @@ func convertTrackBytes(bytes []byte) (track *SMFTrack, err error) {
 			d1, d2 := byte(0), byte(0)
 			d1, bytes, err = takeByte(bytes)
 			if err != nil {
-				errmsg1 := "convertTrackBytes error while creating Channel Message"
-				errmsg2 := "status = 0x%02X, channel = %d,  index = %d"
-				err = pigerr.CompoundError(err, errmsg1, fmt.Sprintf(errmsg2, byte(runningStatus), byte(runningChannel), index))
+				errmsg := "creating channel message, status = 0x%02X, channel = %d"
+				err = error2(err, fmt.Sprintf(errmsg, byte(runningStatus), byte(runningChannel)))
 				return
 			}
 			index++
 			if dataCount == 2 {
 				d2, bytes, err = takeByte(bytes)
 				if err != nil {
-					errmsg1 := "convertTrackBytes error while reading 2nd Channel Message data byte."
-					errmsg2 := "status = 0x%02X, channel = %d,  index = %d"
-					err = pigerr.CompoundError(err, errmsg1, fmt.Sprintf(errmsg2, byte(runningStatus), byte(runningChannel), index))
+					errmsg := "getting channel message second data byte, status = 0x%02X, channel = %d"
+					err = error2(err, fmt.Sprintf(errmsg, byte(runningStatus), byte(runningChannel)))
 					return
 				}
 				index++
@@ -138,9 +145,8 @@ func convertTrackBytes(bytes []byte) (track *SMFTrack, err error) {
 			var ue *UniversalEvent
 			ue, err = MakeChannelEvent(runningStatus, runningChannel, d1, d2)
 			if err != nil {
-				errmsg1 := "convertTrackBytes error while crearting Chanbnel Message"
-				errmsg2 := "status = 0x%02X, channel = %d,  index = %d"
-				err = pigerr.CompoundError(err, errmsg1, fmt.Sprintf(errmsg2, byte(runningStatus), byte(runningChannel), index))
+				errmsg := "creating channel message, status = 0x%02X, channel = %d"
+				err = error2(err, fmt.Sprintf(errmsg, byte(runningStatus), byte(runningChannel)))
 				return
 			}
 			ue.deltaTime = deltaTime
@@ -153,9 +159,8 @@ func convertTrackBytes(bytes []byte) (track *SMFTrack, err error) {
 			} else {
 				ue, err = MakeSystemEvent(StatusByte(status))
 				if err != nil {
-					errmsg1 := "convertTrackBytes error while creating System Message"
-					errmsg2 := "status = 0x%02X,  index = %d"
-					err = pigerr.CompoundError(err, errmsg1, fmt.Sprintf(errmsg2, byte(status), index))
+					errmsg := "creatring sysmte-message, status = 0x%02X"
+					err = error2(err, fmt.Sprintf(errmsg, byte(status)))
 					return
 				}
 				index++
@@ -167,21 +172,21 @@ func convertTrackBytes(bytes []byte) (track *SMFTrack, err error) {
 			var ue *UniversalEvent
 			mtype, bytes, err = takeByte(bytes)
 			if err != nil {
-				errmsg1 := "convertTrackBytes error getting meta-type byte, index = %d"
-				err = pigerr.CompoundError(err, fmt.Sprintf(errmsg1, index))
+				errmsg := "creating meta-type"
+				err = error2(err, errmsg)
 				return
 			}
 			if !isMetaType(mtype) {
-				errmsg := "convertTrackBytes Expected meta type at index %d, got 0x%2X"
-				err = pigerr.New(fmt.Sprintf(errmsg, index, mtype))
+				errmsg := "invalid metaType 0x%02X"
+				err = error1(fmt.Sprintf(errmsg, mtype))
 				return
 			}
 			index++
 			var countVLQ *VLQ
 			countVLQ, bytes, err = takeVLQ(bytes)
 			if err != nil {
-				errmsg1 := "convertTrackBytes error geting meta event count, index = %d"
-				err = pigerr.CompoundError(err, fmt.Sprintf(errmsg1, index))
+				errmsg1 := "invalid meta event count"
+				err = error2(err, errmsg1)
 				return
 			}
 			index += countVLQ.Length()
@@ -198,9 +203,8 @@ func convertTrackBytes(bytes []byte) (track *SMFTrack, err error) {
 				bytes = bytes[count:]
 				ue, err = MakeMetaEvent(MetaType(mtype), data)
 				if err != nil {
-					errmsg1 := "convertTrackBytes error making Meta Message"
-					errmsg2 := "meta type = 0x%02X '%s',  index = %d"
-					err = pigerr.CompoundError(err, errmsg1, fmt.Sprintf(errmsg2, byte(mtype), mtype, index))
+					errmsg := "creating meta message, meta type = 0x%02X"
+					err = error2(err, fmt.Sprintf(errmsg, byte(mtype)))
 					return
 				}
 				index += count
@@ -208,9 +212,8 @@ func convertTrackBytes(bytes []byte) (track *SMFTrack, err error) {
 				events = append(events, ue)
 			}
 		default:
-			errmsg := "Unhandle switch case for convertTrackBytes "
-			errmsg += "index = %d, status = 0x%2X"
-			err = pigerr.New(fmt.Sprintf(errmsg, index, status))
+			errmsg := "Unhandled switch case, status = 0x%02X"
+			err = error1(fmt.Sprintf(errmsg, status))
 			return
 		}
 	}

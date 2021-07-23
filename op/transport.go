33124662eaadadced638bead1c29fc4efecdd4c0
Steven Jones <plewto@gmail.com>
2021-07-23 12:23:43 -0500

    Adds transport OSC response formatting.
    NOTE: These changes were in place prior to BUG 6 being detected.

--- op/transport.go
+++ op/transport.go
@@ -7,6 +7,7 @@ import (
 
 
 type Transport interface {
+	Name() string
 	Stop()
 	Play() error
 	Continue() error
@@ -23,84 +24,117 @@ type Transport interface {
 
 func initTransportHandlers(transport Transport) {
 
-	// stop
-	// play
-	// continue
-	// load
-	// enable-midi-transport flag
-	// q-midi-transport-enabled
-	// q-is-playing
-	// q-duration
-	// q-position
 
+	formatResponse := func(command string, values ...string) []string {
+		acc := make([]string, 0, len(values) + 1)
+		acc = append(acc, fmt.Sprintf("subcommand = %s.%s", transport.Name(), command))
+		for _, v := range values {
+			acc = append(acc, v)
+		}
+		return acc
+	}
+
+	formatErrorResponse := func(command string) []string {
+		acc := []string{fmt.Sprintf("subcommand = %s.%s", transport.Name(), command)}
+		return acc
+	}
+			
+	
+	// /pig/op <name>, stop
+	//
 	remoteStop := func(msg *goosc.Message) ([]string, error) {
 		var err error
 		transport.Stop()
-		return empty, err
+		return  formatResponse("stop"), err
 	}
 
+	// /pig/op <name>, play
+	//
 	remotePlay := func(msg *goosc.Message) ([]string, error) {
-		var err error
-		transport.Play()
-		return empty, err
+		err := transport.Play()
+		if err != nil {
+			rs := formatErrorResponse("play")
+			return rs, err
+		}
+		rs := formatResponse("play", transport.MediaFilename())
+		return rs, err
 	}
 
+	// /pig/op <name>, continue
+	//
 	remoteContinue := func(msg *goosc.Message) ([]string, error) {
-		var err error
-		transport.Continue()
-		return empty, err
+		err := transport.Continue()
+		if err != nil {
+			return formatErrorResponse("continue"), err
+		}
+		return formatResponse("continue"), err
 	}
 
-	// op <id>, load, <filename>
+	// /pig/op <name> load <filename>
+	//
 	remoteLoad := func(msg *goosc.Message) ([]string, error) {
 		value, err := ExpectMsg("oss", msg)
 		if err != nil {
-			return empty, err
+			return formatErrorResponse("load"), err
 		}
 		filename := value[2].S
 		err = transport.LoadMedia(filename)
-		return []string{filename}, err
+		if err != nil {
+			return formatErrorResponse("load"), err
+		}
+		return formatResponse("load", filename), err
 	}
 
-	// op <id>, enable-midi-transport, <bool>
+	// op <name>, enable-midi-transport, <bool>
+	//
 	remoteEnableMIDITransport := func(msg *goosc.Message) ([]string, error) {
 		value, err := ExpectMsg("osb", msg)
 			if err != nil {
-			return empty, err
+				return formatErrorResponse("enable-midi-transport"), err
 			}
 		flag := value[2].B
 		transport.EnableMIDITransport(flag)
-		return empty, err
+		return formatResponse("enable-midi-transport", fmt.Sprintf("%v", flag)), err
 	}
 
+	// op <name> q-midi-transport-enabled  --> bool
+	//
 	remoteQueryMIDITransport := func(msg *goosc.Message) ([]string, error) {
 		var err error
-		flag := fmt.Sprintf("%v", transport.MIDITransportEnabled)
-		return []string{flag}, err
+		flag := fmt.Sprintf("%v", transport.MIDITransportEnabled())
+		return formatResponse("q-midi-transport-enabled", fmt.Sprintf("%v", flag)), err
 	}
-	
+
+	// op <name> q-is-playing  --> bool
+	//
 	remoteQueryIsPlaying := func(msg *goosc.Message) ([]string, error) {
 		var err error
 		flag := fmt.Sprintf("%v", transport.IsPlaying())
-		return []string{flag}, err
+		return formatResponse("q-is-playing", fmt.Sprintf("%v", flag)), err
 	}
 
+	// op <name> q-duration  --> time(msec)
+	//
 	remoteQueryDuration := func(msg *goosc.Message) ([]string, error) {
 		var err error
-		dur := fmt.Sprintf("%f", transport.Duration())
-		return []string{dur}, err
-	}
-			
-	remoteQueryPosition := func(msg *goosc.Message) ([]string, error) {
-		var err error
-		pos := fmt.Sprintf("%f", transport.Position())
-		return []string{pos}, err
+		dur := fmt.Sprintf("%d", transport.Duration())
+		return formatResponse("q-duration", dur), err
 	}
 
+	// op <name> q-position  --> time(msec)
+	//
+	remoteQueryPosition := func(msg *goosc.Message) ([]string, error) {
+		var err error
+		pos := fmt.Sprintf("%d", transport.Position())
+		return formatResponse("q-position", pos), err
+	}
+
+	// op <name> q-media-filename  --> filename
+	//
 	remoteQueryMediaName := func(msg *goosc.Message) ([]string, error) {
 		var err error
 		name := transport.MediaFilename()
-		return []string{name}, err
+		return formatResponse("q-media-name", name), err
 	}
 
 	
@@ -109,10 +143,8 @@ func initTransportHandlers(transport Transport) {
 	transport.addCommandHandler("play", remotePlay)
 	transport.addCommandHandler("continue", remoteContinue)
 	transport.addCommandHandler("load", remoteLoad)
-
 	transport.addCommandHandler("enable-midi-transport", remoteEnableMIDITransport)
 	transport.addCommandHandler("q-midi-transport-enabled", remoteQueryMIDITransport)
-		
 	transport.addCommandHandler("q-is-playing", remoteQueryIsPlaying)
 	transport.addCommandHandler("q-duration", remoteQueryDuration)
 	transport.addCommandHandler("q-position", remoteQueryPosition)
